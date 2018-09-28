e9a8fba0d88487f517af67b262cc9d36e4ef9e1c
dhoarau20 <42975628+dhoarau20@users.noreply.github.com>
2018-09-28 12:27:38 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,22 @@
+
+public class OperationsTest 
+{
+
+	public static void main(String[] args)
+	{
+		int number; 
+		number = 20;
+		number = number + 5;
+		System.out.println(number);
+		number/=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+
+	}
+
+}
